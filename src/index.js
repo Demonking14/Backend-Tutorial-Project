01ffe1ec22757bf645e43dd5bbc64f711c22a704
Demonking14 <adi.jha1424@gmail.com>
2025-02-04 23:01:13 +0530

    hmm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import Connection from "./db/Connect.js";
+// import mongoose from "mongoose";
 
 dotenv.config({
     path : "./.env"
 })
 
 Connection();
+/*
+different approch to connect mongodb
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`).then(() => {
+            console.log(`\nMongoDB Connection Successfully on Name : ${process.env.DB_NAME}`)
+        })
+    } catch (error) {
+        console.log("Eroor in Connection is " ,error)
+    }
+})()
+*/
